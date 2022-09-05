2dd0fbde222ac31ca6d467a38274df67711bb45d
Abdurahim1234567890 <dzhanbakkiev@mail.ru>
2022-09-05 18:28:22 +0600

    updata inline

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ import storage
 from aiogram import Bot, Dispatcher
 from decouple import config
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 storage = MemoryStorage()
+
 TOKEN = config('TOKEN')
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
-
-
-ADMIN = [661518487, 894749556, 5661093553]
+ADMIN = [661518487]
+URL = 'https://newsuperbot.herokuapp.com/'
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import random
 
 from aiogram.utils import executor
-from config import dp
+from config import dp, URL, bot
 from handlers import client, callbak, extra, admin, fsm_menu, natification, inline
 from database import bot_db
 import logging
 import asyncio
-
+from decouple import config
 
 async def on_startup(_):
+    await bot.set_webhook(URL)
     asyncio.create_task(natification.time())
     bot_db.sql_create()
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
 inline.register_handler_inline(dp)
 fsm_menu.register_handler_fsmmenu(dp)
 client.register_handlers_client(dp)
@@ -24,6 +29,15 @@ extra.register_handler_extra(dp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    #executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path="",
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host='0.0.0.0',
+        port=config("PORT", cast=int)
+    )
 
 #1
