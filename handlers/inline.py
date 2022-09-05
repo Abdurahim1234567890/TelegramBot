b76fd53224537f125313c2f235fb285721ca8722
Abdurahim1234567890 <dzhanbakkiev@mail.ru>
2022-09-05 17:46:23 +0600

    added wiki inline

---
+++ handlers/inline.py
@@ -0,0 +1,29 @@
+import hashlib
+from youtube_search import YoutubeSearch as YT
+from aiogram import types, Dispatcher
+
+
+def finder(text):
+    results = YT(text, max_results=10).to_dict()
+    return results
+
+
+async def inliane_wikipedia_handler(query: types.InlineQuery, link=None):
+    text = query.query or "echo"
+    link = f"https://ru.wikipedia.org/wiki/{text}"
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    articles = [
+        types.InlineQueryResultArticle(
+            id=result_id,
+            title="Wiki: ",
+            url=link,
+            input_message_content=types.InputMessageContent
+            (message_text=link
+             )
+        )
+    ]
+    await query.answer(articles, cache_time=60, is_personal=True)
+
+
+def register_handler_inline(dp: Dispatcher):
+    dp.register_inline_handler(inliane_wikipedia_handler)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 from aiogram.utils import executor
 from config import dp
-from handlers import client, callbak, extra, admin, fsm_menu, natification
+from handlers import client, callbak, extra, admin, fsm_menu, natification,inline
 from database import bot_db
 import logging
 import asyncio
@@ -11,13 +11,14 @@ import asyncio
 async def on_startup(_):
     asyncio.create_task(natification.time())
     bot_db.sql_create()
-
+inline.register_handler_inlane(dp)
 fsm_menu.register_handler_fsmmenu(dp)
 client.register_handlers_client(dp)
 callbak.register__handlers_callback(dp)
 
 admin.register_handler_admin(dp)
 natification.register_hendler_notification(dp)
+
 
 extra.register_handler_extra(dp)
 
